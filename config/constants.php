a4816c0d8c62d22bb3611b9051a7e257d76156e7
Katorazua <116016680+Katorazua@users.noreply.github.com>
2024-08-09 15:45:45 +0100

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define ('ROOT_URL', 'http://localhost/projectchaha/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'Alpha');
+define('DB_PASS', 'alpha@management.services');
+define('DB_NAME', 'projectchaha');
